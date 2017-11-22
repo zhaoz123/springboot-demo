1fe5452d66c9a98f7c359a1e05ed4d81790205e2
zhangxiaodi <644637012@qq.com>
2017-11-22 17:28:46 +0800

    1111

--- springboot-learning-example-master/chapter-2-spring-boot-quick-start/src/main/java/spring/boot/core/QuickStartApplication.java
+++ springboot-learning-example-master/chapter-2-spring-boot-quick-start/src/main/java/spring/boot/core/QuickStartApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuickStartApplication {
 
 	public static void main(String[] args) {
+		System.out.println("aaaaaa");
 		SpringApplication.run(QuickStartApplication.class, args);
 	}
 }
