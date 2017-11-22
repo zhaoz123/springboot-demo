d21c1488c04690c5a38b9d46139574ad1cae9b9c
unknown <suncff@qq.com>
2017-11-22 17:22:41 +0800

    modify QuickStart

--- springboot-learning-example-master/chapter-2-spring-boot-quick-start/src/main/java/spring/boot/core/QuickStartApplication.java
+++ springboot-learning-example-master/chapter-2-spring-boot-quick-start/src/main/java/spring/boot/core/QuickStartApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuickStartApplication {
 
 	public static void main(String[] args) {
+		
+		SpringApplication.run(QuickStartApplication.class, "111");
+
 		SpringApplication.run(QuickStartApplication.class, args);
 	}
 }
